c270b5a110374176e823668d977f534118984a0c
AKIHIDEEgashira <egashira.akihide@gmail.com>
2023-06-14 14:08:03 -0400

    feature: add new reset password logic

--- app/_components/Button/Logout.tsx
+++ app/_components/Button/Logout.tsx
@@ -1,6 +1,7 @@
 import { auth } from '@/app/_firebase/firebase';
-import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
+import { ArrowLeftOnRectangleIcon, ArrowPathIcon } from '@heroicons/react/24/outline'
 import { useSignOut } from 'react-firebase-hooks/auth';
+import toast from 'react-hot-toast';
 
 export function LogoutButton() {
   const [signOut, loading, error] = useSignOut(auth);
@@ -8,16 +9,17 @@ export function LogoutButton() {
   const handleLogout = async () => {
     try {
       await signOut();
-      console.log('success to logout');
-    } catch (error) {
+      toast.success('logout successfully');
+    } catch (e) {
       console.error(error);
+      toast.error('error occured')
     }
   }
 
   return (
     <button className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange' onClick={handleLogout}>
       <div className='w-6 h-6'>
-        <ArrowLeftOnRectangleIcon />
+        {loading ? <ArrowPathIcon /> : <ArrowLeftOnRectangleIcon />}
       </div>
     </button>
   )
--- app/_components/Modal/(Auth)/ResetPassword.tsx
+++ app/_components/Modal/(Auth)/ResetPassword.tsx
@@ -1,9 +1,32 @@
 import { PrimaryButton } from "@/app/_components/Button/Primary";
 import { FormItem } from "@/app/_components/Form/Item";
+import { auth } from "@/app/_firebase/firebase";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
 
 export function AuthResetPassword() {
+  const [email, setEmail] = useState('');
+  const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }
+
+  const handleReset = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await sendPasswordResetEmail(email);
+      toast.success('sent password reset email successfuly ');
+    } catch(e) {
+      console.error(error);
+      toast.error('error occured');
+    }
+  }
+
   return (
-    <form className='space-y-6 px-6 py-4'>
+    <form className='space-y-6 px-6 py-4' onSubmit={handleReset}>
       <h3 className='text-xl font-medium text-white'>reset password</h3>
       <p className='text-sm text-white'>
         Forgotten your password? Enter your e-mail address below, and we&apos;ll send you an e-mail allowing you to reset it.
@@ -13,10 +36,11 @@ export function AuthResetPassword() {
         labelText='your email'
         inputType='email'
         placeholder='name@company.com'
+        onChange={(e) => handleChange(e)}
       />
 
       <PrimaryButton
-        text='reset password'
+        text={sending ? 'sending...' : 'reset password'}
         type='submit'
       />
     </form>
